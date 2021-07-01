07a99811f514cd1dbd4141ae05fb53be9f0994ca
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-07-01 17:31:21 -0300

    Add files via upload

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -7,10 +7,13 @@ function email(){
   let usuario = text.value.substring(0, text.value.indexOf("@"));
   let dominio = text.value.substring(text.value.indexOf("@")+ 1, text.value.length);
 
-if ((usuario.length >=1) && (dominio.length >=3) && (usuario.search("@")==-1) && (dominio.search("@")==-1) && (usuario.search(" ")==-1) && (dominio.search(" ")==-1) && (dominio.search(".")!=-1) && (dominio.indexOf(".") <= 2) && (dominio.lastIndexOf(".") < dominio.length - 1)) {
+if ((usuario.length >=1) && (dominio.length >=3) && (usuario.search("@")==-1) && (dominio.search("@")==-1) && (usuario.search(" ")==-1) && (dominio.search(" ")==-1) && (dominio.search(".")!=-1) && (dominio.indexOf(".") >=1) && (dominio.lastIndexOf(".") < dominio.length - 1)) {
 alert("E-mail valido");
+text.classList.add("right")
 }
 else{
 alert("E-mail invalido");
+text.classList.add("wrong")
+
 }
 }
