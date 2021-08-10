aeb85948f1116ded5cdd79090e2f7487d6fbc7bc
nicolasdsa <69026498+nicolasdsa@users.noreply.github.com>
2021-08-09 23:41:48 -0300

    Add files via upload

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,17 +1,40 @@
 let bill = document.querySelector(".bill");
 let button = document.querySelectorAll(".button");
 let person = document.querySelector(".person")
+let total = document.querySelector(".total");
+let amount = document.querySelector(".amount");
+let custom = document.querySelector(".custom");
+
+amount.innerHTML = "0.00";
+total.innerHTML = "0.00";
 
 button.forEach((event) => event.addEventListener("click", teste))
-
+custom.addEventListener("change", Tipcustom)
 
 function teste(event) {
   const tip = parseInt(event.target.innerText.replace("%", ""));
-  console.log(person.value);
-  if (person.value < 0) {
+  const people = parseInt(person.value);
+  if (people < 0 || isNaN(people) == true) {
     console.log("0 nao pode")
   }
   else {
-    console.log("maior que zero")
+    const costtotal =  ((bill.value * tip )/ 100).toFixed(2);
+    const divide = (costtotal / people).toFixed(2);
+    amount.innerHTML = divide;
+    total.innerHTML = costtotal;
+  }
+}
+
+function Tipcustom(){
+  const tip = parseInt(custom.value);
+  const people = parseInt(person.value);
+  if (people < 0 || isNaN(people) == true) {
+    console.log("0 nao pode custom")
+  }
+  else {
+    const costtotal =  ((bill.value * tip )/ 100).toFixed(2);
+    const divide = (costtotal / people).toFixed(2);
+    amount.innerHTML = divide;
+    total.innerHTML = costtotal;
   }
 }
