a2bc608e146e65932db72cbcd9d64a6c62c7f016
Nicolas Sosa Machado <nicolassosamachado25@gmail.com>
2022-04-05 23:54:57 -0300

    Finished script and html, Almost finished style.css

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -2,11 +2,22 @@ let thanks = document.querySelector(".thanks")
 let rating = document.querySelector(".rating");
 let button = document.querySelector(".button");
 let listLi = document.querySelectorAll(".list > li");
+let noteRating = document.querySelector(".noteRating")
 
 button.addEventListener("click", handleClick);
+listLi.forEach((event) => event.addEventListener("click", Teste));
 
 function handleClick(){
     rating.classList.toggle("hidden");
     thanks.classList.toggle("hidden");
 }
 
+function Teste(e){
+    LiCss(e);
+    noteRating.textContent = e.target.textContent;
+}
+
+function LiCss(e){
+    listLi.forEach(e => e.classList.remove("active"));
+    e.target.classList.add("active");
+}
